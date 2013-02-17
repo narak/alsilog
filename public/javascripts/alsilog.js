83817608f0edc3459e0134a5bac1e80ed4a3546c
Karan Sheth <karan@abkya.com>
2013-02-17 17:51:17 +0530

    historified it, painful

--- public/javascripts/alsilog.js
+++ public/javascripts/alsilog.js
@@ -6,16 +6,26 @@ require.config({
 	shim: {
     	underscore: {
   			exports: '_'
+    	},
+    	history: {
+    		exports: 'History'
     	}
 	}
 });
 
-require(['require', 'jquery', 'underscore'], function(require, $, _) {
+require(['require', 'jquery', 'underscore', 'history'], function(require, $, _, History) {
 	// Template tag for _.
 	_.templateSettings = {
 	    interpolate: /\{\{\=(.+?)\}\}/gim,
 	    evaluate: /\{\{(.+?)\}\}/gim
 	};
+
+	// Bind to StateChange Event
+    History.Adapter.bind(window,'statechange',function(){ // Note: We are using statechange instead of popstate
+        var state = History.getState(); // Note: We are using History.getState() instead of event.state
+        History.log(state.data, state.title, state.url);
+        ajaxify(state.data);
+    });
 
 	var view = (function() {
 		var exports = {},
@@ -56,14 +66,23 @@ require(['require', 'jquery', 'underscore'], function(require, $, _) {
 		return exports;
 	})();
 
+	var ajaxify = function(state) {
+		$.getJSON(state.url, function(data) {
+			view.render(state.type, state.target, data);
+		});
+		return false;
+	};
+
     $(function() {
     	$(document).on('click', 'a.ajaxify', function(e) {
     		e.preventDefault();
     		var $this = $(this);
-    		$.getJSON($this.attr('href'), function(data) {
-    			view.render($this.data('type'), $this.data('target'), data);
-    		});
-			return false;
+    		var state = {
+    			url: $this.attr('href'),
+    			type: $this.data('type'),
+    			target: $this.data('target')
+    		};
+			History.pushState(state, $this.attr('title'), state.url);
     	});
     });
 });
