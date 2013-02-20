413bc1920a7881818898c682e0b3fc5c4c255468
Karan Sheth <karan@abkya.com>
2013-02-20 22:03:12 +0530

    change how undefined is checked

--- public/javascripts/alsilog.js
+++ public/javascripts/alsilog.js
@@ -1,99 +1,102 @@
 require.config({
-	baseUrl: "javascripts",
-	paths: {
-	    "underscore": "http://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.4.4/underscore"
-	},
-	shim: {
-    	underscore: {
-  			exports: '_'
-    	},
-    	history: {
-    		exports: 'History'
-    	}
-	}
+    baseUrl: "javascripts",
+    paths: {
+        "underscore": "http://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.4.4/underscore"
+    },
+    shim: {
+        underscore: {
+            exports: '_'
+        },
+        history: {
+            exports: 'History'
+        }
+    }
 });
 
-require(['require', 'jquery', 'underscore', 'history'], function(require, $, _, History) {
-	// Template tag for _.
-	_.templateSettings = {
-	    interpolate: /\{\{\=(.+?)\}\}/gim,
-	    evaluate: /\{\{(.+?)\}\}/gim
-	};
+require(['require', 'jquery', 'underscore', 'history'], function (require, $, _, History) {
+    'use strict';
 
-	// Bind to StateChange Event
-    History.Adapter.bind(window,'statechange',function() {
+    // Template tag for _.
+    _.templateSettings = {
+        interpolate: /\{\{\=(.+?)\}\}/gim,
+        evaluate: /\{\{(.+?)\}\}/gim
+    };
+
+    // Bind to StateChange Event
+    History.Adapter.bind(window, 'statechange', function () {
         var state = History.getState();
         ajaxify(state.url, state.data);
     });
 
-	var view = (function() {
-		var exports = {},
-			tplCache = {},
-			elCache = {},
-			defaultTarget = '#main-content',
-			titlePrepend = 'alsilog',
-			titleSep = ' | ';
-		var getTemplate = function(name, callback) {
-			if (typeof tplCache[name] === 'undefined') {
-				require(['text!templates/' + name + '.html'], function(html) {
-					tplCache[name] = _.template(html);
-					callback(tplCache[name]);
-				});
-			} else {
-				callback(tplCache[name]);
-			}
-		};
+    var view = (function () {
+        var exports = {},
+            tplCache = {},
+            elCache = {},
+            defaultTarget = '#main-content',
+            titlePrepend = 'alsilog',
+            titleSep = ' | ';
 
-		var getEl = function(el) {
-			if (typeof elCache[el] === 'undefined') {
-				elCache[el] = $(el);
-			}
-			return elCache[el];
-		};
+        var getTemplate = function (name, callback) {
+            if (tplCache[name] === undefined) {
+                require(['text!templates/' + name + '.html'], function (html) {
+                    tplCache[name] = _.template(html);
+                    callback(tplCache[name]);
+                });
+            } else {
+                callback(tplCache[name]);
+            }
+        };
 
-		exports.render = function(view, target, data) {
-			if (typeof target === 'undefined') {
-				target = defaultTarget;
-			}
-			getTemplate(view, function(template) {
-				var $el = getEl(target);
-				$el.fadeOut('fast', function() {
-					if (typeof data.title !== 'undefined') {
-						document.title = titlePrepend + titleSep + data.title;
-					} else {
-						document.title = titlePrepend;
-					}
-					getEl(target).html(template(data));
-					$el.fadeIn('fast');
-				});
-			});
-		};
+        var getEl = function (el) {
+            if (elCache[el] === undefined) {
+                elCache[el] = $(el);
+            }
+            return elCache[el];
+        };
 
-		return exports;
-	})();
+        exports.render = function (view, target, data) {
+            if (target === undefined) {
+                target = defaultTarget;
+            }
+            getTemplate(view, function (template) {
+                var $el = getEl(target);
+                $el.fadeOut('fast', function () {
+                    if (data.title !== undefined) {
+                        document.title = titlePrepend + titleSep + data.title;
+                    } else {
+                        document.title = titlePrepend;
+                    }
+                    getEl(target).html(template(data));
+                    $el.fadeIn('fast');
+                });
+            });
+        };
 
-	var urlDataCache = {};
-	var ajaxify = function(url, state) {
-		if (typeof urlDataCache[url] === 'undefined') {
-			$.getJSON(url, function(data) {
-				urlDataCache[url] = data;
-				view.render(data.view, state.target, data.content);
-			});
-		} else {
-			var data = urlDataCache[url];
-			view.render(data.view, state.target, data.content);
-		}
-	};
+        return exports;
+    })();
 
-    $(function() {
-    	$(document).on('click', 'a.ajaxify', function(e) {
-    		e.preventDefault();
-    		var $this = $(this);
-    		var state = {
-    			type: $this.data('type'),
-    			target: $this.data('target')
-    		};
-			History.pushState(state, $this.attr('title'), $this.attr('href'));
-    	});
+    var urlDataCache = {};
+    var ajaxify = function (url, state) {
+        if (urlDataCache[url] === undefined) {
+            $.getJSON(url, function (data) {
+                urlDataCache[url] = data;
+                view.render(data.view, state.target, data.content);
+            });
+        } else {
+            var data = urlDataCache[url];
+            view.render(data.view, state.target, data.content);
+        }
+    };
+
+    $(function () {
+        $(document).on('click', 'a.ajaxify', function (e) {
+            e.preventDefault();
+            var $this = $(this);
+            var state = {
+                type: $this.data('type'),
+                target: $this.data('target')
+            };
+            History.pushState(state, $this.attr('title'), $this.attr('href'));
+        });
     });
 });
